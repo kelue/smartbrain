cd479abedee3c8a054913760c7533db2dabb2ca7
kelue <austincmoon5@gmail.com>
2022-04-12 22:09:23 +0100

    completed facial recognition functionality

--- src/components/FacialRecognition/FacialRecognition.js
+++ src/components/FacialRecognition/FacialRecognition.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './FacialRecognition.css';
 
-const FacialRecognition = ({imageUrl}) => {
+const FacialRecognition = ({imageUrl, box}) => {
     return(
         <div className="center ma">
             <div className="absolute mt2">
-                <img src={imageUrl} alt="face-detect" width="500px" height="auto" />
+                <img id="inputImage" src={imageUrl} alt="face-detect" width="500px" height="auto" />
+                <div className="bounding_box" style={{top: box.topRow,  right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     )
