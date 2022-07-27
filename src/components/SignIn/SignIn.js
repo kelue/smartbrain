a94a8c8fbd31ddaba677a0ffc53146e2da69ae91
kelue <austincmoon5@gmail.com>
2022-07-27 05:50:31 +0100

    update image count feature

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -28,8 +28,10 @@ class SignIn extends Component {
             })
         })
         .then(response => response.json())
-        .then(data => {
-            if(data === 'success'){
+        .then(user => {
+            if(user.id)
+            {
+                this.props.loadUser(user)
                 this.props.onRouteChange('home')
             }
         })
